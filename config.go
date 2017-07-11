f7d2ab506d8c7408e8fcf59c2af17bfa97659a68
YoRHa No.2 Type B <unit.2b@yorha.army>
2017-07-11 22:15:14 +0800

    add config.go and config_test.go; README.md updated

---
+++ config.go
@@ -0,0 +1,107 @@
+package exiles
+
+import (
+	"errors"
+	"net/url"
+	"strings"
+)
+
+var (
+	// SupportedCiphers array of supported AEAD ciphers, uppercased
+	SupportedCiphers = []string{
+		"AEAD_CHACHA20_POLY1305",
+	}
+	// CipherAliases aliases of AEAD ciphers, case-ignored
+	CipherAliases = map[string]string{
+		"CHACHA20-POLY1305":      "AEAD_CHACHA20_POLY1305",
+		"CHACHA20-IETF-POLY1305": "AEAD_CHACHA20_POLY1305",
+	}
+)
+
+var (
+	// ErrBadURL URL is mal-formatted
+	ErrBadURL = errors.New("bad ss:// url")
+	// ErrBadScheme URL scheme is not 'ss'
+	ErrBadScheme = errors.New("scheme is not 'ss' in ss:// url")
+	// ErrBadCipher Cipher is not in SupportedCiphers
+	ErrBadCipher = errors.New("cipher is not supported in ss:// url, only " + strings.Join(SupportedCiphers, ",") + " are supported")
+	// ErrMissingPasswd password is missing from url
+	ErrMissingPasswd = errors.New("password is not specified in ss:// url")
+	// ErrMissingAddress host:port is missing from url
+	ErrMissingAddress = errors.New("host:port is not specified in ss:// url")
+)
+
+// Config represents a basic configuration with address, cipher and password
+type Config struct {
+	// full address of ss protocol, for both server and client
+	Address string
+	// AEAD cipher, see https://www.iana.org/assignments/aead-parameters/aead-parameters.xhtml for names
+	// default to "AEAD_CHACHA20_POLY1305"
+	Cipher string
+	// password
+	Passwd string
+}
+
+// ParseConfigFromURL decode url string to Config
+// Format:
+//	ss://CIPHER:PASSWORD@ADDRESS:PORT
+func ParseConfigFromURL(urlstr string) (*Config, error) {
+	config := Config{}
+	u, err := url.Parse(urlstr)
+	// check URL
+	if err != nil {
+		return nil, ErrBadURL
+	}
+	// check scheme
+	if u.Scheme != "ss" {
+		return nil, ErrBadScheme
+	}
+	// check CIPHER:PASSWORD
+	if u.User == nil {
+		return nil, ErrMissingPasswd
+	}
+	// extract cipher, passwd
+	passwd, ok := u.User.Password()
+	if ok {
+		// extract both cipher and passwd
+		config.Cipher = u.User.Username()
+		config.Passwd = passwd
+	} else {
+		// if no ciper, use default
+		config.Cipher = SupportedCiphers[0]
+		config.Passwd = u.User.Username()
+	}
+	// check is cipher supported
+	config.Cipher, ok = ValidateCipher(config.Cipher)
+	if !ok {
+		return nil, ErrBadCipher
+	}
+	// check passwd
+	if len(config.Passwd) == 0 {
+		return nil, ErrMissingPasswd
+	}
+	// check address
+	config.Address = u.Host
+	if len(config.Address) == 0 {
+		return nil, ErrMissingAddress
+	}
+	return &config, nil
+}
+
+// ValidateCipher resolve cipher alises and check if cipher is supported
+func ValidateCipher(cipher string) (string, bool) {
+	// to uppercase
+	d := strings.ToUpper(cipher)
+	// check supported ciphers
+	for _, s := range SupportedCiphers {
+		if s == d {
+			return d, true
+		}
+	}
+	// check aliases
+	a := CipherAliases[d]
+	if len(a) != 0 {
+		return a, true
+	}
+	return "", false
+}
---
+++ config_test.go
@@ -0,0 +1,34 @@
+package exiles
+
+import (
+	"testing"
+)
+
+func TestParseConfig(t *testing.T) {
+	// should error
+	c, e := ParseConfigFromURL("http://sadfdasf")
+	if c != nil || e != ErrBadScheme {
+		t.Errorf("should fail on bad scheme; config: %v; error: %v", c, e)
+	}
+	c, e = ParseConfigFromURL("ss://hello")
+	if c != nil || e != ErrMissingPasswd {
+		t.Errorf("should fail on missing passwd; config: %v; error: %v", c, e)
+	}
+	c, e = ParseConfigFromURL("ss://what:what@hello")
+	if c != nil || e != ErrBadCipher {
+		t.Errorf("should fail on bad cipher; config: %v; error: %v", c, e)
+	}
+	// should ok
+	c, e = ParseConfigFromURL("ss://what@hello")
+	if c == nil || e != nil || c.Passwd != "what" || c.Cipher != SupportedCiphers[0] {
+		t.Errorf("should ok with default cipher; config: %v; error: %v", c, e)
+	}
+	c, e = ParseConfigFromURL("ss://chacha20-polY1305:what@hello")
+	if c == nil || e != nil || c.Passwd != "what" || c.Cipher != "AEAD_CHACHA20_POLY1305" {
+		t.Errorf("should ok with aliases; config: %v; error: %v", c, e)
+	}
+	c, e = ParseConfigFromURL("ss://AEAD_CHACHA20_POLY1305:what@hello")
+	if c == nil || e != nil || c.Passwd != "what" || c.Cipher != "AEAD_CHACHA20_POLY1305" {
+		t.Errorf("should ok with specified cipher; config: %v; error: %v", c, e)
+	}
+}
